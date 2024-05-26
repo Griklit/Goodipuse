dd4c45dcc43df2c1c33066b00037525980e3c892
Sequence[7] <sequence7@scalt.sct>
2024-05-26 22:13:55 +0800

    清理无用内容

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,12 @@
 <script>
 	import './styles.css';
-	import {page} from "$app/stores";
 </script>
 
 <div class="app">
 	<nav>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-		</ul>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
 	</nav>
-
 	<main>
 		<slot />
 	</main>
