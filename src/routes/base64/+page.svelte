59ea9ea7665fb9bac39e6b46be0b6741d73b97d6
YongHuM1ng <1277651550@qq.com>
2024-05-26 23:36:37 +0800

    ...

--- src/routes/base64/+page.svelte
+++ src/routes/base64/+page.svelte
@@ -1,26 +1,27 @@
 <svelte:head>
-	<title>Base64&更多</title>
-	<meta name="description" content="About this app" />
+    <title>Base64</title>
+    <meta name="description" content="About this app"/>
 </svelte:head>
 
-<div class="text-column">
-	<h1>About this app</h1>
+<section>
+    <div>
+        <input type="text" bind:value={decodedText} on:input={encode}/>
+        <img src="data:image/png;base64,{encodedText}" alt="base64"/>
+    </div>
+    <div>
+        <input type="text" bind:value={encodedText} on:input={decode}/>
+    </div>
+</section>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
+<script lang="ts">
+    let decodedText: string = '';
+    let encodedText: string = '';
 
-	<pre>npm create svelte@latest</pre>
+    function encode() {
+        encodedText = btoa(decodedText)
+    }
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div>
+    function decode() {
+        decodedText = atob(encodedText)
+    }
+</script>
