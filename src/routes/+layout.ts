e09a984b8717d993cd831e4e7b5d52361aafdc80
Sequence[7] <sequence7@scalt.sct>
2024-05-29 14:33:14 +0800

    we need js

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,7 @@
-import { browser } from '$app/environment'
+import {browser} from '$app/environment'
 import '$lib/i18n' // Import to initialize. Important :)
-import { locale, waitLocale } from 'svelte-i18n'
-import type { LayoutLoad } from './$types'
+import {locale, waitLocale} from 'svelte-i18n'
+import type {LayoutLoad} from './$types'
 
 export const load: LayoutLoad = async () => {
     if (browser) {
@@ -9,3 +9,4 @@ export const load: LayoutLoad = async () => {
     }
     await waitLocale()
 }
+export const prerender = true;
