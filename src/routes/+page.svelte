992a801af6c16328623f3f60b8e6bcc91b79cd3e
YongHuM1ng <1277651550@qq.com>
2024-06-04 23:00:37 +0800

    i18n

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,11 +19,11 @@
     import {page} from "$app/stores";
 
     let routers = new Map<string, { title: string }>();
-    routers.set('/substitution/caesar', {title: $_('module.substitution-caesar.title')});
-    routers.set('/substitution/a1z26', {title: $_('module.substitution-a1z26.title')});
-    routers.set('/substitution/custom', {title: $_('module.substitution-custom.title')});
-    routers.set('/base64/image', {title: $_('module.base64-image.title')});
-    routers.set('/external/software', {title: $_('module.external-software.title')});
+    routers.set('/substitution/caesar', {title: $_('module.substitution.caesar.title')});
+    routers.set('/substitution/a1z26', {title: $_('module.substitution.a1z26.title')});
+    routers.set('/substitution/custom', {title: $_('module.substitution.custom.title')});
+    routers.set('/base64/image', {title: $_('module.base64.image.title')});
+    routers.set('/external/software', {title: $_('module.external.software.title')});
     routers.set('/playground', {title: 'playground'});
 </script>
 
