ee90982676b0f91fd671c6c4164aeac26687256a
Sequence[7] <sequence7@scalt.sct>
2024-06-05 15:13:54 +0800

    逐步加载

--- src/routes/image/steganography/+page.svelte
+++ src/routes/image/steganography/+page.svelte
@@ -161,7 +161,7 @@
 
     async function enlarge(index: number) {
         canvasList[index][2] = !canvasList[index][2];
-        await new Promise(resolve => setTimeout(resolve, 1));
+        await new Promise(resolve => setTimeout(resolve, 0));
         canvasList[index][0]?.scrollIntoView({behavior: "smooth", block: "start", inline: "center"});
     }
 
@@ -178,6 +178,7 @@
             let imageData = ctx.getImageData(0, 0, image.width, image.height);
             await handler.renderer(imageData.data);
             ctx.putImageData(imageData, 0, 0);
+            await new Promise(resolve => setTimeout(resolve, 0));
         }
     }
 
