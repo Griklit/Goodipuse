6c94b0dabac83743260ec1d99afe45a2a512f26e
Sequence[7] <sequence7@scalt.sct>
2024-06-05 16:15:29 +0800

    主页样式调整

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
     <h1>{$_('page.home.description')}</h1>
     <nav>
         {#each routers as [path, router]}
-            <a class="navigate-card" class:selected={$page.url.pathname === path} href={path}>
+            <a class="win11-ui-card-surface-can-press" class:selected={$page.url.pathname === path} href={path}>
                 {router.title}
             </a>
         {/each}
@@ -17,21 +17,28 @@
 <script lang="ts">
     import {_} from 'svelte-i18n';
     import {page} from "$app/stores";
+    import '$lib/styles/win11-ui/card.css'
 
     let routers = new Map<string, { title: string }>();
     routers.set('/substitution/caesar', {title: $_('module.substitution.caesar.title')});
     routers.set('/substitution/a1z26', {title: $_('module.substitution.a1z26.title')});
     routers.set('/substitution/custom', {title: $_('module.substitution.custom.title')});
     routers.set('/base64/image', {title: $_('module.base64.image.title')});
+    routers.set('image/steganography', {title: $_('module.image.steganography.title')});
     routers.set('/external/software', {title: $_('module.external.software.title')});
-    routers.set('/playground', {title: 'playground'});
 </script>
 
 <style>
     nav {
-        display: flex;
-        flex-direction: column;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
         padding: 1rem 0;
         gap: 1rem;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+        text-align: center;
     }
 </style>
