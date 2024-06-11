b843e0fda982e6d280d268dea8227683eb59a24b
seq7 <sequence7@scalt.sct>
2024-06-11 16:51:46 +0800

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,8 @@
     routers.set('/substitution/custom', {title: $_('module.substitution.custom.title')});
     routers.set('/number/base', {title: $_('module.number.base.title')});
     routers.set('/base64/image', {title: $_('module.base64.image.title')});
-    routers.set('image/steganography', {title: $_('module.image.steganography.title')});
+    routers.set('/encoding/morse', {title: $_('module.encoding.morse.title')});
+    routers.set('/image/steganography', {title: $_('module.image.steganography.title')});
     routers.set('/external/software', {title: $_('module.external.software.title')});
 </script>
 
