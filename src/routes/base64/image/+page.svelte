1b66ac94b6f83c867fad9200b780129f2a97ed6e
seq7 <sequence7@scalt.sct>
2024-06-11 14:45:12 +0800

    b64

--- src/routes/base64/image/+page.svelte
+++ src/routes/base64/image/+page.svelte
@@ -6,14 +6,14 @@
 <section>
     <textarea placeholder="Base64 encoded text" bind:value={encodedText} on:input={decode}/>
     <div>
-        {#if mediaType.startsWith('text/')}
-            <p>{decodedText}</p>
-        {:else if mediaType.startsWith('image/')}
+        {#if mediaType?.startsWith('image/')}
             <img src={encodedText} alt="base64"/>
-        {:else if mediaType.startsWith('audio/')}
-            <audio src={encodedText}/>
-        {:else if mediaType.startsWith('video/')}
-            <video src={encodedText}/>
+        {:else if mediaType?.startsWith('audio/')}
+            <audio controls src={encodedText}/>
+        {:else if mediaType?.startsWith('video/')}
+            <video controls src={encodedText}>
+                <track kind="captions" src="">
+            </video>
         {:else}
             <p>{$_('module.base64.image.invalid')}</p>
         {/if}
@@ -24,18 +24,13 @@
     import {_} from 'svelte-i18n';
 
     let encodedText: string = '';
-    let mediaType: string = '';
-    let decodedText: string = '';
+    let mediaType: string | null = null;
 
     function decode() {
         if (!encodedText.startsWith('data:')) {
-            mediaType = 'text/plain';
+            mediaType = null;
         } else {
             mediaType = encodedText.split(';')[0].split(':')[1];
-        }
-        if (mediaType.startsWith('text/')) {
-            console.log(encodedText);
-            decodedText = atob(encodedText.split(',')[1]);
         }
     }
 </script>
@@ -51,5 +46,10 @@
     textarea {
         min-width: 16rem;
         width: 64rem;
+        min-height: 24rem;
+    }
+
+    div {
+        align-self: center;
     }
 </style>
